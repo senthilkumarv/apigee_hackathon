c9d2abc815dc0780016e6a08ba971ee655ebbb62
Rajaram <rmallya@thougthworks.com>
2013-03-02 15:50:28 +0530

    fixed getSources api

--- js/redbus.js
+++ js/redbus.js
@@ -1,38 +1,26 @@
 var redBus = function(){
 
     function authUrl(url, params){
+
+        params = params ||{};
     
-    var oauthObject = OAuthSimple().sign({path:url,
-                                          parameters:params,
-                                          signatures:{
-                                            api_key:"ANwhpxc7owtm3HurEaxVHvC3HS1qqz",
-                                            shared_secret:"bO7y2b68jlxr5UMVoGDw1d5phOjpj8"
-                                          }
-                                        });
-    return oauthObject.signed_url;
+        var oauthObject = OAuthSimple().sign({path:url,
+                                              parameters:params,
+                                              signatures:{
+                                                api_key:"ANwhpxc7owtm3HurEaxVHvC3HS1qqz",
+                                                shared_secret:"bO7y2b68jlxr5UMVoGDw1d5phOjpj8"
+                                              }
+                                            });
+        return oauthObject.signed_url;
     }
 
-    function getSources(){
-        
-        
-
-        $.ajax(authUrl("http://api.seatseller.travel/sources"),{
-
-            headers:{
-                oauth_consumer_key:"ANwhpxc7owtm3HurEaxVHvC3HS1qqz",
-                oauth_signature_method:"HMAC-SHA1",
-                oauth_timestamp:"1362205866",
-                oauth_nonce:"503674127",
-                oauth_version:"1.0",
-                oauth_signature:"niVM2LKxoAgI2vVMqEvRqqJvp%2Bc%3D"
-            },
-
+    function getSources(callback){
+       
+        var url = authUrl("http://api.seatseller.travel/sources");
+        $.ajax(url,{
             success:function(data){
-                console.log(data);
+                callback(data);
             },
-
-
-            dataType:"jsonp"
 
         });
     }
